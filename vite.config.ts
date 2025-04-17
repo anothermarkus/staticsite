8d827f0fec4517a111d607364e2813060aabb8bd
Kopec <mark.kopec@dell.com>
2025-04-16 20:53:14 -0500

    fixing fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src', // tells Vite to use ./src/index.html as entry
+  publicDir: '../public', // relative to vite.config.js
   build: {
     outDir: '../dist',
     emptyOutDir: true,
